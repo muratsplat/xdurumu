ab39ab93bb36e06168e0f92770869fa6d754d325
Murat Ödünç <murat.asya@gmail.com>
2015-08-10 21:15:27 +0300

    adda new migrations and for it model

---
+++ app/WeatherHourlyStats.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App;
+
+use App\CacheAbleEloquent; 
+
+class WeatherHourlyStats extends CacheAbleEloquent
+{
+   
+     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'weather_hourly_stats';
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */            
+    protected $fillable = ['enable', 'source_updated_at', 'dt'];    
+    
+    
+        /**
+         * To define an inverse one-to-one relationship 
+         * 
+         * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+         */
+        public function city() 
+        {
+            return $this->belongsTo('App\City', 'city_id', 'id');
+        }
+
+        /**
+         * To define an many to many polymorphic relation
+         * 
+         * @return \Illuminate\Database\Eloquent\Relations\MorphToMany
+         */
+        public function conditions()
+        {
+            return $this->morphToMany('App\WeatherCondition', 'weather_condition_able', 'weather_condition_ables');            
+        }
+
+        /**
+         * To define an inverse one-to-one relationship 
+         * 
+         * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+         */
+        public function foreCastResource() 
+        {
+            return $this->belongsTo('App\WeatherForeCastResource', 'weather_forecast_resource_id', 'id');
+        }
+
+        /**
+         * TO define an one to one relationship
+         * 
+         * @return \Illuminate\Database\Eloquent\Relations\HasOne
+         */
+        public function main() 
+        {            
+            return $this->hasOne('App\WeatherMain', 'weather_hourly_id','id');
+        }
+        
+        /**
+         * TO define an one to one relationship
+         * 
+         * @return \Illuminate\Database\Eloquent\Relations\HasOne
+         */
+        public function wind() 
+        {            
+            return $this->hasOne('App\WeatherWind', 'weather_hourly_id','id');
+        }       
+
+        
+        /**
+         * TO define an one to one relationship
+         * 
+         * @return \Illuminate\Database\Eloquent\Relations\HasOne
+         */
+        public function clouds() 
+        {            
+            return $this->hasOne('App\WeatherCloud', 'weather_hourly_id','id');
+        }
+        
+        /**
+         * TO define an one to one relationship
+         * 
+         * @return \Illuminate\Database\Eloquent\Relations\HasOne
+         */
+        public function sys() 
+        {            
+            return $this->hasOne('App\WeatherSys', 'weather_hourly_id','id');
+        }  
+    
+}
---
+++ database/migrations/2015_08_10_200915_create_weatherHourly_table.php
@@ -0,0 +1,67 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWeatherHourlyTable extends Migration
+{
+    protected $table = "weather_hourly_stats";
+    
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        
+//        {"city":{"id":1851632,"name":"Shuzenji",
+//        "coord":{"lon":138.933334,"lat":34.966671},
+//        "country":"JP",
+//        "cod":"200",
+//        "message":0.0045,
+//        "cnt":38,
+//        "list":[{
+//                "dt":1406106000,
+//                "main":{
+//                    "temp":298.77,
+//                    "temp_min":298.77,
+//                    "temp_max":298.774,
+//                    "pressure":1005.93,
+//                    "sea_level":1018.18,
+//                    "grnd_level":1005.93,
+//                    "humidity":87
+//                    "temp_kf":0.26},
+//                "weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04d"}],
+//                "clouds":{"all":88},
+//                "wind":{"speed":5.71,"deg":229.501},
+//                "sys":{"pod":"d"},
+//                "dt_txt":"2014-07-23 09:00:00"}
+//                ]}
+        
+        \Schema::create($this->table, function(Blueprint $t) {
+            
+            $t->increments('id');
+            $t->integer('city_id')->unsigned();        
+            $t->integer('weather_forecast_resource_id')->unsigned()->nullable();          
+            $t->boolean('enable')->default(true);
+            $t->timestamp('source_updated_at')->nullable();
+            $t->dateTime('dt')->nullable();      
+            $t->timestamps();             
+         
+            $t->foreign('city_id')->references('id')->on('cities');             
+            $t->foreign('weather_forecast_resource_id')->references('id')->on('weather_forecast_resources');   
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        \Schema::drop($this->table);
+    }
+}
