35eba0017fc5fa31f4fe7bc6ede446d69462fa18
Murat Ödünç <murat.asya@gmail.com>
2015-08-13 13:53:02 +0300

    adds new repository class to manege WeatherList model

---
+++ app/Repositories/Weather/WeatherListRepository.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Repositories\Weather;
+
+//use App\WeatherHourlyStat as Hourly;
+//use App\WeatherCondition as Condition; 
+//use App\Libs\Weather\DataType\WeatherDataAble;
+//use App\Repositories\CacheAbleRepository as CacheAble;
+//use Illuminate\Contracts\Cache\Repository as Cache;
+//use Illuminate\Contracts\Config\Repository as Config;
+//use ErrorException;
+
+/**
+ * Weather Hourly Stats Repository Class
+ * 
+ * @package WeatherForcast
+ */
+class WeatherListRepository extends CacheAble
+{    
+    /**
+     * @var \App\WeatherList
+     */
+    private $mainModel;
+    
+    
+            /**
+         * To get main model which is injected
+         * 
+         * @return \Illuminate\Database\Eloquent\Model
+         */
+        public function onModel()
+        {
+            
+        }
+        
+        /**
+         * To get all models from cache drive
+         * 
+         * return \Illuminate\Database\Eloquent\Collection|static[]
+         */
+        public function onCache()
+        {
+            
+        }
+        
+        
+        
+        /**
+         * To get all models
+         * 
+         * @param bool $cache
+         * @return \Illuminate\Database\Eloquent\Collection|static[]
+         */
+        public function all()
+        {
+            
+        }
+        
+}
