32e45f58b91d131342ed58cb2c4002ae71b23bab
Murat Ödünç <murat.asya@gmail.com>
2015-08-28 11:18:15 +0300

    adds new files about controllers ands assets for template

---
+++ app/Http/Controllers/Home.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+//use Illuminate\Http\Request;
+
+//use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+/**
+ * A controller for main domain..
+ * 
+ */
+class Home extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        return view('welcome');
+    }   
+}
---
+++ resources/views/front/weather/home.blade.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Hava Durumu | durumum.NET | Hayatı Kolaylaştıran Uygulamalar</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">   
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
+  <body class="hold-transition skin-blue layout-top-nav">
+    <div class="wrapper">
+
+      <header class="main-header">
+        <nav class="navbar navbar-static-top">
+          <div class="container">
+            <div class="navbar-header">
+              <a href="http://{{config('app.domain')}}" class="navbar-brand"><b>durumum</b>.NET</a>
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+                <i class="fa fa-bars"></i>
+              </button>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+                <li><a href="#">Link</a></li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li class="divider"></li>
+                    <li><a href="#">Separated link</a></li>
+                    <li class="divider"></li>
+                    <li><a href="#">One more separated link</a></li>
+                  </ul>
+                </li>
+              </ul>
+              <form class="navbar-form navbar-left" role="search">
+                <div class="form-group">
+                  <input type="text" class="form-control" id="navbar-search-input" placeholder="Search">
+                </div>
+              </form>
+            </div><!-- /.navbar-collapse -->
+
+          </div><!-- /.container-fluid -->
+        </nav>
+      </header>
+      <!-- Full Width Column -->
+      <div class="content-wrapper">
+        <div class="container">
+          <!-- Content Header (Page header) -->
+          <section class="content-header">
+            <h1>
+              Top Navigation
+              <small>Example 2.0</small>
+            </h1>
+            <ol class="breadcrumb">
+              <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+              <li><a href="#">Layout</a></li>
+              <li class="active">Top Navigation</li>
+            </ol>
+          </section>
+
+          <!-- Main content -->
+          <section class="content">
+            <div class="callout callout-info">
+              <h4>Tip!</h4>
+              <p>Add the layout-top-nav class to the body tag to get this layout. This feature can also be used with a sidebar! So use this class if you want to remove the custom dropdown menus from the navbar and use regular links instead.</p>
+            </div>
+            <div class="callout callout-danger">
+              <h4>Warning!</h4>
+              <p>The construction of this layout differs from the normal one. In other words, the HTML markup of the navbar and the content will slightly differ than that of the normal layout.</p>
+            </div>
+            <div class="box box-default">
+              <div class="box-header with-border">
+                <h3 class="box-title">Blank Box</h3>
+              </div>
+              <div class="box-body">
+                The great content goes here
+              </div><!-- /.box-body -->
+            </div><!-- /.box -->
+          </section><!-- /.content -->
+        </div><!-- /.container -->
+      </div><!-- /.content-wrapper -->
+      <footer class="main-footer">
+        <div class="container">
+          <div class="pull-right hidden-xs">
+            <b>Version</b> 2.3.0
+          </div>
+          <strong>Copyright &copy; 2014-2015 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights reserved.
+        </div><!-- /.container -->
+      </footer>
+    </div><!-- ./wrapper -->
+    
+    @include('front._cdn_boostrap_font_awesome_jquery');
+
+    <!-- Theme style and Other independent css files -->
+    <link rel="stylesheet" href="{{ elixir('assets/front/weather/css/all.css') }}">   
+    <!-- JS -->
+    <!-- SlimScroll, FastClick, AdminLTE App -->    
+    <script src="{{ elixir('assets/front/weather/js/libs.js') }}"></script>      
+
+    @include('_ga')    
+  </body>
+</html>
